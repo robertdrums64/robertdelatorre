3eb638e7b3fc151e719ed91a6e0f5324cb348a2a
Robert De La Torre <robertdrums64@gmail.com>
2019-05-31 15:24:47 -0700

    Finished the logo and gif! FUN STUFF

--- js/svg.js
+++ js/svg.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
     // alert("Hello!");
+    var rdInner = $('.rd-inner');
+    var outer = $('#outer');
+    // var gifImages = $('#gif-images');
+    var gifImages = $('#background-images img');
+
+    outer.hover(function(){
+        // alert("HI!");
+        rdInner.fadeToggle(500, 0.0);
+    });
 
 });
