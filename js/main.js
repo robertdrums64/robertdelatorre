97d282329d6d06a44c3c2d774684d20ac93af940
Robert De La Torre <robertdrums64@gmail.com>
2019-05-31 16:56:53 -0700

    Added the links to the social sites and started with the menu icons in main js.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    var cowbell = $("#cowbell");
+    var drumtop = $("#drumtop");
+    var sticks = $("#sticks");
+    var cymbal = $("#cymbal");
+
+    cowbell.click(function(){
+        alert('HI!');
+    });
+    drumtop.click(function(){
+        alert('HI!');
+    });
+    sticks.click(function(){
+        alert('HI!');
+    });
+    cymbal.click(function(){
+        alert('HI!');
+    });
+
+});
--- js/svg.js
+++ js/svg.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     var outer = $('#outer');
     // var gifImages = $('#gif-images');
     var gifImages = $('#background-images img');
+    var gifLogo = $('.main-logo-cont');
 
     outer.hover(function(){
         // alert("HI!");
