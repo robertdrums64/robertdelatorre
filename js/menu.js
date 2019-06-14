38d71609c4c38658ace9ef4a9d399cf46fe4fa42
Robert De La Torre <robertdrums64@gmail.com>
2019-06-14 16:47:30 -0700

    Added the links and gallery.

--- js/menu.js
+++ js/menu.js
@@ -3,23 +3,18 @@ $(document).ready(function(){
     const drumtop = $("#drumtop");
     const sticks = $("#sticks");
     const cymbal = $("#cymbal");
-    const home = $("");
-
     const content = $(".main-content-wrap");
     
     cowbell.click(function(){
-        $(content).load('./pages/about.html');
+        $(content).load('./pages/content.html');
     });
     drumtop.click(function(){
         $(content).load('./pages/gallery.html');
     });
     sticks.click(function(){
-        $(content).load('./pages/contact.html');
-    });
-    cymbal.click(function(){
         $(content).load('./pages/events.html');
     });
-    home.click(function(){
-        $(content).load('./pages/content.html');
+    cymbal.click(function(){
+        $(content).load('./pages/contact.html');
     });
 });
